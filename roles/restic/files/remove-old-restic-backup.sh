dbd34764d1fee7c855daa5905df8ce083b1b8da8
Joseph Dioquino <sepdioquino08@gmail.com>
2021-07-26 21:47:00 +0800

    updated restic forget file

--- roles/restic/files/remove-old-restic-backup.sh
+++ roles/restic/files/remove-old-restic-backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-restic forget -w 1 -p /opt/scripts/secret -r /nfs-share/backup/docker-data_node-2
-restic prune -p /opt/scripts/secret -r /nfs-share/backup/docker-data_node-2
+restic forget -w 1 -p /opt/scripts/secret -r /nfs-share/backup/docker-data_{{ ansible_hostname }}
+restic prune -p /opt/scripts/secret -r /nfs-share/backup/docker-data_{{ ansible_hostname }}
