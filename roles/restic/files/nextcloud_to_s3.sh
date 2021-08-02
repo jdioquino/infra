845b1ea8e4ff2c8ddbf29695552e9feedbd638c6
Joseph Dioquino <sepdioquino08@gmail.com>
2021-08-02 10:51:53 +0800

    set mariadb image version to 10.5

--- roles/restic/files/nextcloud_to_s3.sh
+++ roles/restic/files/nextcloud_to_s3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-restic -p /opt/scripts/secret -r {{ s3_nextcloud }} backup /docker-data/nextcloud/
+restic -p /opt/scripts/secret -r {{ s3_nextcloud }} backup /docker-data/nextcloud/ && wget http://hea.{{ domain_name }}/ping/{{ hc_id3 }} && rm -rf {{ hc_id3 }}
